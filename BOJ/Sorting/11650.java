9b5de28768b704b3ad7b61de108a07f3ec37af6b
naekang <66901361+naekang@users.noreply.github.com>
2021-07-06 15:35:05 +0900

    BOJ 1181 java

--- BOJ/Sorting/11650.java
+++ BOJ/Sorting/11650.java
@@ -18,7 +18,7 @@ public class Main {
             if (a[0] == b[0]) {
                 return a[1] - b[1];
             } else {
-                return a[0] = b[0];
+                return a[0] - b[0];
             }
         });
 
