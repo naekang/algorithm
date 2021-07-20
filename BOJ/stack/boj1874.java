98e214d13731a9f6c684d1f2a97b5cc542ae1f7a
naekang <66901361+naekang@users.noreply.github.com>
2021-07-20 18:43:03 +0900

    BOJ 1874 java

---
+++ BOJ/stack/boj1874.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class boj1874 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+
+        int N = sc.nextInt();
+
+        int start = 0;
+
+        while (N-- > 0) {
+            int value = sc.nextInt();
+
+            if (value > start) {
+                for (int i = start + 1; i <= value; i++) {
+                    stack.push(i);
+                    sb.append('+').append('\n');
+                }
+                start = value;
+            } else if (stack.peek() != value) {
+                System.out.println("NO");
+                return;
+            }
+
+            stack.pop();
+            sb.append("-").append("\n");
+        }
+        System.out.println(sb);
+    }
+}
