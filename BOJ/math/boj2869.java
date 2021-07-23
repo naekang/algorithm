0e4a300657e3fe485818167effa89e6e4dbb02c3
naekang <66901361+naekang@users.noreply.github.com>
2021-07-23 11:33:52 +0900

    BOJ 2869 java

---
+++ BOJ/math/boj2869.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class boj2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int day = (V - B) / (A - B);
+
+        if ((V - B) % (A - B) != 0) {
+            day++;
+        }
+
+        System.out.println(day);
+    }
+}
