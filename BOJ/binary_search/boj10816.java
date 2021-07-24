4a2c5abf220c5982d80b9e73e81d473bdf10f913
naekang <66901361+naekang@users.noreply.github.com>
2021-07-24 17:33:38 +0900

    BOJ 10816 java

--- BOJ/binary_search/boj10816.java
+++ BOJ/binary_search/boj10816.java
@@ -1,5 +1,37 @@
-public class boj10816 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 
+public class boj10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String[] str;
+
+        int N = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        str =  br.readLine().split(" ");
+        for (int i = 0; i < N; ++i) {
+            int key = Integer.parseInt(str[i]);
+            if (map.containsKey(key)) {
+                map.put(key, map.get(key) + 1);
+            } else {
+                map.put(key, 1);
+            }
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        str =  br.readLine().split(" ");
+        for (int i = 0; i < M; ++i) {
+            int key = Integer.parseInt(str[i]);
+            if (map.containsKey(key)) {
+                sb.append(map.get(key)).append(' ');
+            } else {
+                sb.append(0).append(' ');
+            }
+        }
+
+        System.out.println(sb);
     }
 }
