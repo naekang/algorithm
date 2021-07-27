b8b0ab44988fa1217bc32b9541b97d99b451bcf0
naekang <66901361+naekang@users.noreply.github.com>
2021-07-27 21:44:15 +0900

    BOJ 1654 java

---
+++ BOJ/binary_search/boj1654.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class boj1654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int K = sc.nextInt();
+        long N = sc.nextLong();
+        long[] arr = new long[K];
+        long high = 0;
+        long low = 1;
+
+        for (int i = 0; i < K; i++) {
+            arr[i] = sc.nextLong();
+            high = Math.max(high, arr[i]);
+        }
+
+
+        while (low <= high) {
+            long cnt = 0;
+            long mid = (low + high) / 2;
+            for (int i = 0; i < K; i++) {
+                cnt += (arr[i] / mid);
+            }
+            if (cnt >= N) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        System.out.println(high);
+    }
+}
