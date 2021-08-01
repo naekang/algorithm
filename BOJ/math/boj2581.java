e678fdfd4eaa00fdc3c6e4e62e5a419882528c04
naekang <66901361+naekang@users.noreply.github.com>
2021-08-01 14:24:51 +0900

    BOJ 2581 java

---
+++ BOJ/math/boj2581.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj2581 {
+
+    static boolean[] prime;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        prime = new boolean[N + 1];
+        primeCheck();
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = M; i <= N; i++) {
+            if (prime[i] == false) {
+                sum += i;
+                if (min == Integer.MAX_VALUE) {
+                    min = i;
+                }
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+
+    }
+
+    static void primeCheck() {
+        prime[0] = true;
+        prime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if (prime[i]) {
+                continue;
+            }
+            for (int j = i * i; j < prime.length; j+= i) {
+                prime[j] = true;
+            }
+        }
+
+    }
+}
