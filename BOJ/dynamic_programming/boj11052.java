2a668a098b86359a565723396cfccf32b6180a08
naekang <66901361+naekang@users.noreply.github.com>
2021-09-11 13:57:37 +0900

    BOJ 11052 java

---
+++ BOJ/dynamic_programming/boj11052.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj11052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N + 1];
+        int[] dp = new int[N + 1];
+        dp[0] = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + arr[j]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
