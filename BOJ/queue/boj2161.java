6a456dc4bb9bc8135c00741d30c9c531b6596770
naekang <66901361+naekang@users.noreply.github.com>
2021-09-12 22:12:47 +0900

    BOJ 2161 java

---
+++ BOJ/queue/boj2161.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class boj2161 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        while (!queue.isEmpty()) {
+            sb.append(queue.poll()).append(" ");
+
+            if (queue.isEmpty())
+                break;
+
+            queue.offer(queue.poll());
+        }
+
+        System.out.println(sb.toString());
+    }
+}
