633d14fa8d93cf7005657caec8d73aec6cfcbbd3
naekang <66901361+naekang@users.noreply.github.com>
2021-09-21 11:45:10 +0900

    BOJ 2743 java

---
+++ BOJ/string/boj2743.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        System.out.println(input.length());
+    }
+}
