2a5a6b4e33826b0f8227f44af55c9dc8be9d82d1
naekang <66901361+naekang@users.noreply.github.com>
2021-10-04 14:40:16 +0900

    BOJ 11652 java

---
+++ BOJ/Sorting/boj11652.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class boj11652 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        long[] arr = new long[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        long num = arr[0];
+        int cnt = 1;
+        int max = 1;
+
+        for (int i = 1; i < N; i++) {
+            if (arr[i] == arr[i - 1]) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+            if (cnt > max) {
+                max = cnt;
+                num = arr[i];
+            }
+        }
+        System.out.println(num);
+    }
+}
