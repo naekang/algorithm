d6db5e774224630ee806bdd4dfc6b77d7868a6a4
naekang <66901361+naekang@users.noreply.github.com>
2021-10-10 15:21:34 +0900

    BOJ 11000 java

---
+++ BOJ/Sorting/boj11000.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class boj11000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(input[0]);
+            arr[i][1] = Integer.parseInt(input[1]);
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                }
+                return o1[0] - o2[0];
+            }
+        });
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(arr[0][1]);
+
+        for (int i = 1; i < N; i++) {
+            if (queue.peek() <= arr[i][0]) {
+                queue.poll();
+            }
+            queue.add(arr[i][1]);
+        }
+
+        System.out.println(queue.size());
+
+    }
+}
