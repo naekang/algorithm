328f2800ad1ccf327b148259e1c18542c06bba87
naekang <66901361+naekang@users.noreply.github.com>
2021-10-23 02:12:36 +0900

    BOJ 2974 java

---
+++ BOJ/implemetation/boj2974.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj2974 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[5];
+        int tmp = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] >= arr[j + 1]) {
+                    tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+
+                    for (int k = 0; k < arr.length; k++) {
+                        sb.append(arr[k] + " ");
+                    }
+                    sb.append("\n");
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
