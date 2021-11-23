3b2b3dfaab485a93f5b82ccf5d12c5dc074be187
naekang <66901361+naekang@users.noreply.github.com>
2021-11-23 15:18:14 +0900

    BOJ 10807 java

---
+++ BOJ/implemetation/boj10807.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj10807 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int cnt = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int v = Integer.parseInt(br.readLine());
+
+        for (int num : arr) {
+            if (v == num) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
