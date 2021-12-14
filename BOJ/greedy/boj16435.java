e2b5e741c2b7c02057f9d58b97614b8db432bca7
naekang <66901361+naekang@users.noreply.github.com>
2021-12-14 11:06:23 +0900

    BOJ 16435 java

---
+++ BOJ/greedy/boj16435.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj16435 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int ans = L;
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= ans) {
+                ans += 1;
+            } else {
+                break;
+            }
+        }
+        System.out.println(ans);
+    }
+}
