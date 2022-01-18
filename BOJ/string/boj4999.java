20aa6396780e1f1eea59e4df6318bfc71222d254
naekang <rlawlsgh6306@gmail.com>
2022-01-19 08:16:20 +0900

    BOJ 4999 java

---
+++ BOJ/string/boj4999.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj4999 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        System.out.println((str1.length() >= str2.length()) ? "go" : "no");
+    }
+}
