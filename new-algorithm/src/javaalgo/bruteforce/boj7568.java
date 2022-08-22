6ef16530f9cad6a1665ff5aa5d22f35e2ff5a640
naekang <rlawlsgh6306@gmail.com>
2022-08-22 23:16:06 +0900

    java baekjoon

---
+++ new-algorithm/src/javaalgo/bruteforce/boj7568.java
@@ -0,0 +1,34 @@
+package javaalgo.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj7568 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            int cnt = 1;
+            for (int j = 0; j < N; j++) {
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    cnt++;
+                }
+            }
+            sb.append(cnt).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+}
